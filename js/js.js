a6c5c4172de98f9b7115bb802ba85b2d69322af1
RATINDN <ratindn@gmail.com>
2025-02-19 23:58:24 +0330

    r

--- js/js.js
+++ js/js.js
@@ -160,6 +160,92 @@ const mode2 = document.getElementById('mode2' );
 
 
 
+// function darkmode(){
+//   const element = document.body;
+//   const nav = document.getElementById('nav');
+//   const bow = document.getElementById('bow');
+//   const reg = document.getElementById('register');
+//   const scroll2 = document.getElementById('scroll');
+//   const button1 = document.getElementById("button-read1");
+//   const button2 = document.getElementById("button-read2");
+//   const button3 = document.getElementById("button-read3");
+//   const moon = document.getElementById("moon");
+//   const sun = document.getElementById("sun");
+//   const li1 = document.getElementById("li1");
+//   const li2 = document.getElementById("li2");
+//   const btn = document.getElementById("closebtn");
+//   const veil2 = document.getElementById("veil2");
+//   const lb1 = document.getElementById("lb1");
+//   const lb2 = document.getElementById("lb2");
+//   const boxing1 = document.getElementById("boxing1");
+//   const boxing2 = document.getElementById("boxing2");
+//   const boxing3 = document.getElementById("boxing3");
+//   const boxing4 = document.getElementById("boxing4");
+//   const boxing5 = document.getElementById("boxing5");
+//   const boxing6 = document.getElementById("boxing6");
+//   const boxing7 = document.getElementById("boxing7");
+//   const search_box = document.getElementById("search-box")
+//   const search=document.getElementById("search")
+//   const search_box_2 = document.getElementById("search-box-2")
+//   const search_2 = document.querySelector(".search-input")
+
+
+
+
+
+
+
+
+
+//   sun.classList.toggle("const-sun");
+//   moon.classList.toggle("const-moon");
+// element.classList.toggle("dark-mode");
+// nav.classList.toggle("dark-mode");
+// bow.classList.toggle("const-bow");
+// reg.classList.toggle("const-register");
+// scroll2.classList.toggle("const-scroll");
+// button1.classList.toggle("const-button2");
+// button2.classList.toggle("const-button2");
+// button3.classList.toggle("const-button2");
+// li1.classList.toggle("const-li1");
+// li2.classList.toggle("const-li2");
+// btn.classList.toggle("const-closebtn");
+// veil2.classList.toggle("const-veil2");
+// lb1.classList.toggle("const-lb");
+// lb2.classList.toggle("const-lb");
+// menu.classList.toggle("const-menu");
+// openmenu.classList.toggle("const-open");
+// boxing1.classList.toggle("const-box");
+// boxing2.classList.toggle("const-box");
+// boxing3.classList.toggle("const-box");
+// boxing4.classList.toggle("const-box");
+// boxing5.classList.toggle("const-box");
+// boxing6.classList.toggle("const-box");
+// boxing7.classList.toggle("const-box");
+// search_box.classList.toggle("const-search")
+// search.classList.toggle("const-search-mode")
+// search_box_2.classList.toggle("const-search")
+// search_2.classList.toggle("const-search-mode")
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// }
+
+
 function darkmode() {
   const element = document.body;
   const myMenu = document.getElementById('myMenu');
@@ -419,5 +505,6 @@ document.getElementById("search-mobile").addEventListener("input", function() {
 
 
 
+// alert("Welcome to my shop" )
 
 
