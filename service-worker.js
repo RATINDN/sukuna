e6f5327f91e9400ef9ace3457a1a8e45b5029f79
RATINDN <ratindn@gmail.com>
2025-04-03 22:52:15 +0330

    yes

--- service-worker.js
+++ service-worker.js
@@ -22,33 +22,51 @@ const urlsToCache = [
   '/images/x-lg.svg'
 ];
 
+// Install event: Cache the files
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache); // Add all URLs to cache
+      })
+      .then(() => self.skipWaiting()) // Activate worker immediately
   );
 });
 
+// Fetch event: Serve cached files or fetch from network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Not in cache - return fetch from network
+        return fetch(event.request);
+      }
+    )
   );
 });
 
+// Activate event: Remove old caches
 self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
+
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Delete caches that are not in the whitelist
+            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
       );
     })
+      .then(() => self.clients.claim()) // Take control of all clients
   );
 });
